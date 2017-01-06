45a3c4ff14e9c2ffcd7cb46bd7b62acc74166b9b
rafthab <rafthab@mobileiron.com>
2017-01-06 15:40:29 -0800

    Test

--- src/dataStructures/stack/stackUtils.java
+++
@@ -1,7 +0,0 @@
-package dataStructures.stack;
-
-/**
- * Created by rafthab on 5/16/16.
- */
-public class stackUtils {
-}
---
+++ src/stackUtils.java
@@ -0,0 +1,8 @@
+/**
+ * Created by rafthab on 5/16/16.
+ */
+public class stackUtils {
+    public static void main(String[] args) {
+        System.out.println("poop");
+    }
+}
