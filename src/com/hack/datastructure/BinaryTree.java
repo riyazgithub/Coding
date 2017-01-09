f1c50b753ef63b1df87f7a331d1f6f04149c63d5
rafthab <rafthab@mobileiron.com>
2017-01-09 13:24:34 -0800

    Partially done yay not bad More additions Testcase changes Adding inefficient code works though so i am going to move on mostly Refactoring project Adding Binary Tree implementation .. Ha ha  Its a start i know

---
+++ src/com/hack/datastructure/BinaryTree.java
@@ -0,0 +1,8 @@
+package com.hack.datastructure;
+
+/**
+ * Created by rafthab on 1/9/17.
+ */
+public class BinaryTree {
+
+}
---
+++ src/com/hack/datastructure/TreeNode.java
@@ -0,0 +1,14 @@
+package com.hack.datastructure;
+
+/**
+ * Created by rafthab on 1/9/17.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+
+}
