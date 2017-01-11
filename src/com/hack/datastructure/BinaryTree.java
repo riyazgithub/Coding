a45a2f8460a21d61501c60764ff4208a882ed0af
rafthab <rafthab@mobileiron.com>
2017-01-11 13:19:00 -0800

    Partially done yay not bad More additions Testcase changes Adding inefficient code works though so i am going to move on mostly Refactoring project Committing so far Adding insertleft and insert right

--- src/com/hack/datastructure/BinaryTree.java
+++ src/com/hack/datastructure/BinaryTree.java
@@ -6,15 +6,36 @@ package com.hack.datastructure;
 
 public class BinaryTree {
 
-    TreeNode head;
-     private void insertLeft(TreeNode at, int val) {
+    public static TreeNode head;
+     private TreeNode insertLeft(TreeNode at, int val) {
          at.left = new TreeNode(val);
+         return at.left;
      }
-    private void insertRight(TreeNode at, int val) {
+    private TreeNode insertRight(TreeNode at, int val) {
         at.right = new TreeNode(val);
+        return at.right;
     }
-    public void printTree(TreeNode head) {
-
+    public void printTree(TreeNode pointer) {
+         if(pointer == null) {
+             return;
+         }
+         printTree(pointer.left);
+        System.out.print(pointer.val + " ");
+        printTree(pointer.right);
     }
 
+    public static void main(String[] args) {
+        BinaryTree bt = new BinaryTree();
+        // define head
+        BinaryTree.head = new TreeNode(0);
+        TreeNode one = bt.insertLeft(BinaryTree.head,1);
+        TreeNode two = bt.insertRight(BinaryTree.head,2);
+
+        TreeNode oneone = bt.insertLeft(one, 11);
+        TreeNode onetwo = bt.insertRight(one, 12);
+
+        TreeNode twoone = bt.insertLeft(two, 21);
+
+        bt.printTree(BinaryTree.head);
+    }
 }
