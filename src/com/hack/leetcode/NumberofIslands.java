859b30be414d3e7139200b0e439735f6066aef3b
rafthab <rafthab@mobileiron.com>
2017-01-17 13:30:41 -0800

    Implementing number of islands this should be a good start i think

---
+++ src/com/hack/leetcode/NumberofIslands.java
@@ -0,0 +1,25 @@
+package com.hack.leetcode;
+
+/**
+ * Created by rafthab on 1/17/17.
+ * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
+ * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+ *
+ *
+ * https://leetcode.com/problems/number-of-islands/
+
+ 11000
+ 11000
+ 00100
+ 00011
+ Solution 3
+
+ 11110
+ 11010
+ 11000
+ 00000
+ Solution 1
+
+ */
+public class NumberofIslands {
+}
