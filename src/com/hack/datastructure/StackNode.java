e019357a4d82468af75706cbdf8deb894329b16f
rafthab <rafthab@mobileiron.com>
2017-02-27 19:34:48 -0800

    Basic stack utisl

---
+++ src/com/hack/datastructure/StackNode.java
@@ -0,0 +1,13 @@
+package com.hack.datastructure;
+
+/**
+ * Created by rafthab on 2/27/17.
+ */
+public class StackNode {
+    int val;
+    StackNode next;
+    public StackNode(int value) {
+        this.val = value;
+        this.next = null;
+    }
+}
